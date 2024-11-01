2430996f1343f493cacf52aa306bda6db4b9f5bc
Moojun <mgim32137@gmail.com>
2024-11-01 14:11:15 +0900

    backend 초기 셋업

---
+++ backend/src/test/java/com/dailyhub/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dailyhub.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
